68101acbec60defbcbcb3e402f2c0b53a6d26ee7
William Dantas <wiliam.dantas@pulsati.com.br>
2023-03-12 22:27:15 -0300

    setting http

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _getApiList() {
+    http.get(
+      Uri.https("/api.themoviedb.org", "/4/list/1"),
+      headers: {
+        'Authorization': 'Bearer <<access_token>>',
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+    ).then(
+      (Response value) => print(value.body),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getApiList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
           child: Text('Hello word'),
         ),
